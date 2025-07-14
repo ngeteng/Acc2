b946118c28276fdfc903ca497d108f500f3f9659
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-14 16:25:53 +0700

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const privateKey = process.env.PRIVATE_KEY;
+if (!privateKey) {
+  throw new Error("PRIVATE_KEY tidak ditemukan di file .env");
+}
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    Monad: {
+      url: process.env.RPC_URL_MONAD || "",
+      accounts: [privateKey],
+    },
+    Pharos: {
+      url: process.env.RPC_URL_PHAROS || "",
+      accounts: [privateKey],
+    },
+    Somnia: {
+      url: process.env.RPC_URL_SOMNIA || "",
+      accounts: [privateKey],
+    },
+    OG: {
+      url: process.env.RPC_URL_OG || "",
+      accounts: [privateKey],
+    },
+  },
+};

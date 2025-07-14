fa484ba05e2c763508ecf9dd1e0fd563166c6dc7
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-14 17:32:09 +0700

    Update MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -19,15 +18,11 @@ contract MyNFT is ERC721URIStorage, ERC721Burnable, Ownable {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
-    
-    // ==================================================================
-    // **BAGIAN PERBAIKAN:** Memperbaiki daftar override
-    // ==================================================================
 
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage) // PERBAIKAN DI SINI
+        override(ERC721, ERC721URIStorage)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
@@ -36,7 +31,7 @@ contract MyNFT is ERC721URIStorage, ERC721Burnable, Ownable {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721URIStorage) // PERBAIKAN DI SINI
+        override(ERC721, ERC721URIStorage)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);

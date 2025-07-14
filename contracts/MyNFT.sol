55e3138e1a4569154c964601e47ed9e22149052b
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-14 16:26:29 +0700

    Update MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -7,14 +7,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyNFT is ERC721, Ownable {
     uint256 private _nextTokenId;
 
-    // Saat deploy, kita beri nama dan simbol untuk koleksi NFT
     constructor(string memory name, string memory symbol)
         ERC721(name, symbol)
-        Ownable(msg.sender) // Set pemilik kontrak adalah yang mendeploy
+        Ownable(msg.sender)
     {}
 
-    // Fungsi untuk membuat (mint) NFT baru
-    // Hanya pemilik kontrak yang bisa memanggil fungsi ini
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);

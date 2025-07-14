eda7f642c71cf5c9386f60e269d80abbe8b5a596
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-14 18:36:14 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,31 +25,5 @@ module.exports = {
       url: process.env.RPC_URL_OG || "",
       accounts: [privateKey],
     },
-  },
-  etherscan: {
-    apiKey: {
-      'somnia-testnet': "any_string", // Tidak butuh API key
-      // Tambahkan placeholder untuk jaringan lain jika mereka butuh API key
-      // Pharos: process.env.PHAROS_API_KEY || "", 
-    },
-    customChains: [
-      {
-        network: "somnia-testnet",
-        chainId: 50312,
-        urls: {
-          apiURL: "https://somnia.w3us.site/api",
-          browserURL: "https://somnia.w3us.site"
-        }
-      },
-      // Anda bisa menambahkan konfigurasi untuk jaringan lain di sini
-      // {
-      //   network: "Pharos",
-      //   chainId: 6663,
-      //   urls: {
-      //     apiURL: "https://pharos-testnet.scroll.io/api",
-      //     browserURL: "https://pharos-testnet.scroll.io/"
-      //   }
-      // }
-    ]
   }
 };

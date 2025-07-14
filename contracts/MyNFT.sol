d0c5fe04490cc20bdcf6eac671089b67ba72bde8
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-14 16:59:44 +0700

    Update MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// 1. IMPORT MODUL EKSTENSI URI STORAGE
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721, Ownable {
+// 2. WARISI ERC721URIStorage JUGA
+contract MyNFT is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
 
     constructor(string memory name, string memory symbol)
@@ -15,6 +18,20 @@ contract MyNFT is ERC721, Ownable {
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        _setTokenURI(tokenId, uri); // Fungsi ini sekarang sudah dikenali
+    }
+
+    // Fungsi berikut dibutuhkan oleh ERC721URIStorage
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
